e4707c802a3fc5058233880855d86dfa516267cb
absalomovbiloliddin-cmyk <absalomovbiloliddin@gmail.com>
2026-01-13 19:05:43 +0500

    first

---
+++ script.js
@@ -0,0 +1,37 @@
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const loginBtn = document.getElementById("loginBtn");
+const result = document.getElementById("result");
+
+
+const correctUsername = "Biloliddin";
+const correctPassword = "123456789";
+
+loginBtn.addEventListener("click", function () {
+  const username = usernameInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (username === "" || password === "") {
+    result.classList.remove("hidden");
+    result.style.background = "#ffeaea";
+    result.style.color = "#b00020";
+    result.innerHTML = "Iltimos, barcha maydonlarni to'ldiring";
+    return;
+  }
+
+  if (username === correctUsername && password === correctPassword) {
+    result.classList.remove("hidden");
+    result.style.background = "#eafff5";
+    result.style.color = "#0a7a4a";
+    result.innerHTML = `
+      <strong>Muvaffaqiyatli!</strong><br>
+      Ism: ${username}<br>
+      Parol: ${password}
+    `;
+  } else {
+    result.classList.remove("hidden");
+    result.style.background = "#ffeaea";
+    result.style.color = "#b00020";
+    result.innerHTML = "Login yoki parol noto'g'ri";
+  }
+});
